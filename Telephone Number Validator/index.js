7efe94d07d96f8b6b908fbcffd42fad02e27698e
hamza <hamzaiddou2004@gmail.com>
2025-04-02 22:21:15 +0000

    finish the test and make some extra update to the code

--- Telephone Number Validator/index.js
+++ Telephone Number Validator/index.js
@@ -1,23 +1,29 @@
-const result = document.getElementById(`results-div`);
-const checkbtn = document.getElementById(`check-btn`);
-const clear = document.getElementById(`clear-btn`);
+const result = document.getElementById("results-div");
+const checkbtn = document.getElementById("check-btn");
+const clear = document.getElementById("clear-btn");
 
 function checkPhoneNumber(value) {
     let trimmedValue = value.trim();
     let validPattern = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
-    return validPattern.test(trimmedValue) ? `Valid number: ${trimmedValue}` : `Invalid number: ${trimmedValue}`;
+    return validPattern.test(trimmedValue) 
+        ? `Valid US number: ${trimmedValue}` 
+        : `Invalid US number: ${trimmedValue}`;
 }
 
 checkbtn.addEventListener("click", () => {
-    let inputField = document.getElementById("user-input")
+    let inputField = document.getElementById("user-input");
     let input = inputField.value.trim();
-    checkPhoneNumber(input)
-    result.innerHTML +=  checkPhoneNumber(input) + "<br>";
-    inputField.value = "";
+
+    if (input === "") {
+        alert("Please provide a phone number");
+        return;
+    }
+
+    let resultText = checkPhoneNumber(input);
+    result.innerHTML += resultText + "<br>";
+    inputField.value = ""; 
 });
 
-clear.addEventListener("click", ()=>{
-    result.textContent =  "";
-})
-
-
+clear.addEventListener("click", () => {
+    result.innerHTML = ""; 
+});
