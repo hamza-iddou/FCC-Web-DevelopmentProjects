64bd472538a1de92daf1d9e3b6138113a983a26e
hamza <hamzaiddou2004@gmail.com>
2025-04-04 18:57:01 +0000

    finish now the test

--- Pokémon Search App/script.js
+++ Pokémon Search App/script.js
@@ -1,14 +1,64 @@
 const searchbutton = document.getElementById("search-button");
 const searchinput = document.getElementById(`search-input`);
 let url = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/"
+const hp = document.getElementById(`hp`);
+const attack = document.getElementById(`attack`);
+const defense = document.getElementById(`defense`);
+const special_attack = document.getElementById(`special-attack`);
+const special_defense = document.getElementById(`special-defense`);
+const speed = document.getElementById(`speed`);
+const pokemon_name = document.getElementById(`pokemon-name`);
+const pokemon_id = document.getElementById(`pokemon-id`);
+const weight = document.getElementById(`weight`);
+const height = document.getElementById(`height`);
+const type = document.getElementById(`type`);
+const pimg = document.getElementById(`pokemonimag`);
 
 
 
-
-searchbutton.addEventListener("click", ()=>{
+searchbutton.addEventListener("click", async()=>{
     let id = searchinput.value
     url = url+id
-    fetch(url)
-    .then(response => console.log(response))
-    .catch(error => console.error(error));
+    
+    const data = await fetchdata(url);
+    if(data){
+        renderdata(data)
+        console.log(data)
+    }
 })
+
+
+async function fetchdata(test){
+    try{
+        const response = await fetch(test);
+        if(!response.ok){
+            alert("Pokémon not found")
+            return null;
+        }
+        return await response.json();
+    }
+    catch(error){
+        console.log("Error",error);
+        alert("Something went wrong!");
+        return null;
+    }
+}
+
+function renderdata(data){
+    const stats = data.stats;
+    hp.textContent = stats[0].base_stat;
+    attack.textContent = stats[1].base_stat;
+    defense.textContent = stats[2].base_stat;
+    special_attack.textContent = stats[3].base_stat;
+    special_defense.textContent = stats[4].base_stat;
+    speed.textContent = stats[5].base_stat;
+    pokemon_name.textContent = data.name;
+    pokemon_id.textContent = data.id;
+    weight.textContent =  data.weight;
+    height.textContent = data.height;
+    const types = data.types.map(t => t.type.name).join(", ");
+    type.textContent = types;
+
+    pimg.src = data.sprites.front_default;
+
+}
